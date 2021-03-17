7bed6eaa9ec60166020f46ae8432e57ddaf3b16d
Peter <pgambino5851@gmail.com>
2021-03-17 14:16:12 -0400

    Completed API and front-end components.

---
+++ WebAPIFinal/WebAPIFinal/Controllers/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers.Models
+{
+    public class Department
+    {
+
+        public int DepartmentID { get; set; }
+
+        public string DepartmentName { get; set; }
+    }
+}
